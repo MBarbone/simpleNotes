859b74ab818368cf79e8a77299555c177d0be4e9
Mike <michael.barbone@gmail.com>
2019-07-01 22:05:14 -0400

    Add note working with precautionary code to prevent duplicate notes being added.

--- app.js
+++ app.js
@@ -1,5 +1,79 @@
-const getNotes = require("./notes.js");
+const chalk = require("chalk");
+const yargs = require("yargs");
+const notes = require("./notes");
 
-const msg = getNotes();
+console.log(yargs);
+// Customize Yargs Version
+yargs.version("1.1.0");
 
-console.log(msg);
+// Create add command
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  builder: {
+    title: {
+      describe: "Note Title",
+      demandOption: true,
+      type: "string"
+    },
+    body: {
+      describe: "Note Body",
+      demandOption: true,
+      type: "string"
+    }
+  },
+  showInHelp: true,
+  handler: argv => {
+    notes.addNote(argv.title, argv.body);
+  }
+});
+
+yargs.command({
+  command: "remove",
+  describe: "Remove a note",
+  builder: {
+    title: {
+      describe: "Note Title",
+      demandOption: true,
+      type: "string"
+    }
+  },
+  showInHelp: true,
+  handler: () => {
+    console.log("Removing the note!");
+  }
+});
+
+yargs.command({
+  command: "list",
+  describe: "List all notes",
+  showInHelp: true,
+  builder: {
+    title: {
+      describe: "Note Title",
+      demandOption: true,
+      type: "string"
+    }
+  },
+  handler: () => {
+    console.log("Listing notes!");
+  }
+});
+
+yargs.command({
+  command: "read",
+  describe: "Read a notes",
+  showInHelp: true,
+  builder: {
+    title: {
+      describe: "Note Title",
+      demandOption: true,
+      type: "string"
+    }
+  },
+  handler: () => {
+    console.log("Reading notes!");
+  }
+});
+
+yargs.parse();
--- notes.js
+++ notes.js
@@ -1,5 +1,35 @@
+const fs = require("fs");
 const getNotes = () => {
   return "Your notes...";
 };
 
-module.exports = getNotes;
+const addNote = (title, body) => {
+  const notes = loadNotes();
+  const duplicateNotes = notes.filter(note => {
+    return note.title === title;
+  });
+
+  duplicateNotes.length === 0
+    ? notes.push({ title: title, body: body }) &&
+      console.log(`Note: '${title}' added!`)
+    : console.log("This note already exists");
+
+  saveNotes(notes);
+};
+
+const saveNotes = notes => {
+  const dataJSON = JSON.stringify(notes);
+  fs.writeFileSync("notes.json", dataJSON);
+};
+
+const loadNotes = () => {
+  try {
+    const dataBuffer = fs.readFileSync("notes.json");
+    const dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON);
+  } catch (e) {
+    return [];
+  }
+};
+
+module.exports = { getNotes: getNotes, addNote: addNote };
